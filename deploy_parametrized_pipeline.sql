cfe703b9bc540ca97d7ecb667ae8cfbfd3b3acbb
aabhashsapkota <101363622+aabhashsapkota@users.noreply.github.com>
2024-10-07 17:52:18 -0500

    Update deploy_parametrized_pipeline.sql

--- deploy_parametrized_pipeline.sql
+++ deploy_parametrized_pipeline.sql
@@ -1,3 +1,3 @@
-execute immediate from 'steps/01_setup_snowflake.sql' using (environment => '{{environment}}');
-execute immediate from 'steps/03_harmonize_data.sql' using (environment => '{{environment}}');
-execute immediate from 'steps/04_orchestrate_jobs.sql' using (environment => '{{environment}}', retention_time => {{retention_time}});
+execute immediate from 'steps/01_setup_snowflake.sql';
+execute immediate from 'steps/03_harmonize_data.sql' ;
+execute immediate from 'steps/04_orchestrate_jobs.sql';
